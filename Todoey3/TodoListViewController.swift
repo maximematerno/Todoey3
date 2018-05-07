b8c714fd89a3cae1870fbea472b8869afe9744eb
Ashley Adrias <maximevacher-materno@macbook-air-de-maxime.local>
2018-05-07 14:41:47 -0700

    créer une list plus digne de lapps

---
+++ Todoey3/TodoListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Todoey3
+//
+//  Created by Maxime Vacher-materno on 18-05-07.
+//  Copyright © 2018 Maxime Vacher-materno. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Maxime is a Boss", "Maxime is The best", "Maxime Knows how to dunk"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+        
+    
+
+
+
+
+}
+
